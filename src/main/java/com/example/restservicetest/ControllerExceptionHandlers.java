ebf133ecc5fa812bd9d0f5973f83600a2fc868a2
Arda <arda.dizdaroglu@teamdefinex.com>
2021-08-15 16:19:48 +0300

    added exception handler

---
+++ src/main/java/com/example/restservicetest/ControllerExceptionHandlers.java
@@ -0,0 +1,19 @@
+package com.example.restservicetest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandlers extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { ArrayIndexOutOfBoundsException.class })
+    protected ResponseEntity<Object> arrayIndexOutOfBoundsException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "You entered a larger number than the existing floor number";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE, request);
+    }
+}
