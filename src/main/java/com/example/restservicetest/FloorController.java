6b3201781819d93860bd6e22131dedfabd59eade
Arda <arda.dizdaroglu@teamdefinex.com>
2021-08-15 12:30:14 +0300

    added parking class modified floorcontroller

--- src/main/java/com/example/restservicetest/FloorController.java
+++ src/main/java/com/example/restservicetest/FloorController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class FloorController {
@@ -15,23 +14,10 @@ public class FloorController {
     Random random = new Random();
     int value = random.nextInt(max + min) + min;
 
-    Floor[] floors = new Floor[value];
-
-    public FloorController() {
-
-        int min_height = 1;
-        int max_height = 10;
-        int min_weight = 1;
-        int max_weight = 10;
-
-        for(int i = 0; i< value; i++){
-            floors[i] = new Floor(random.nextInt(max_height + min_height) + min_height,
-                                    random.nextInt(max_weight + min_weight) + min_weight);
-        }
-    }
+    Parking parking = new Parking(value);
 
     @GetMapping("/floor")
     public Floor floor(@RequestParam(value = "id", defaultValue = "0") int id){
-        return floors[id];
+        return parking.getFloor(id);
     }
 }
---
+++ src/main/java/com/example/restservicetest/Parking.java
@@ -0,0 +1,27 @@
+package com.example.restservicetest;
+
+import java.util.Random;
+
+public class Parking {
+
+    private Floor floors[];
+
+    public Parking(int numOfFloors) {
+        floors = new Floor[numOfFloors];
+
+        int min_height = 1;
+        int max_height = 10;
+        int min_weight = 1;
+        int max_weight = 10;
+        Random random = new Random();
+
+        for(int i = 0; i< numOfFloors; i++){
+            floors[i] = new Floor(random.nextInt(max_height + min_height) + min_height,
+                            random.nextInt(max_weight + min_weight) + min_weight);
+        }
+    }
+
+    public Floor getFloor(int index){
+        return floors[index];
+    }
+}
